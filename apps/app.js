f58ad9b1360fedc6b00860a017f3400beb160f57
Tim Bittins <tech@timbittins.com>
2021-10-21 14:28:07 +0200

    finished responsive menu with submenu and mobile button animation

--- apps/app.js
+++ apps/app.js
@@ -1,18 +1,24 @@
 const mobileMenuFunction = () => {
   const mobileBtn = document.querySelector(".mobile-menu-button");
   const mobileMenu = document.querySelector(".mobile-menu");
+  const mobileHam = document.querySelector(".mobile-menu-ham");
+  const mobileCross = document.querySelector(".mobile-menu-cross");
 
   mobileBtn.addEventListener("click", () => {
-    mobileMenu.classList.toggle("-translate-y-full");
+    mobileMenu.classList.toggle("translate-x-full");
+    mobileBtn.classList.toggle("rotate-180");
+    mobileHam.classList.toggle("scale-0");
+    mobileCross.classList.toggle("scale-0");
     console.log("mobilemenu active");
   });
 };
 
 const mobileSubmenuFunction = () => {
   const mobileSubmenuBtn = document.querySelector(".mobile-sub-menu-button");
-  const mobileSubmenuBtnArrow = document.querySelector(".sub-menu-button svg");
+  const mobileSubmenuBtnArrow = document.querySelector(
+    ".mobile-sub-menu-button svg"
+  );
   const mobileSubmenu = document.querySelector(".mobile-sub-menu");
-
 
   mobileSubmenuBtn.addEventListener("click", () => {
     mobileSubmenu.classList.toggle("hidden");
@@ -26,11 +32,16 @@ const lgSubmenuFunction = () => {
   const lgSubmenuBtnArrow = document.querySelector(".lg-sub-menu-button svg");
   const lgSubmenu = document.querySelector(".lg-sub-menu");
 
-
   lgSubmenuBtn.addEventListener("mouseover", () => {
-    lgSubmenu.classList.remove("hidden");
-    lgSubmenuBtnArrow.classList.toggle("rotate-180");
+    lgSubmenu.classList.remove("lg:hidden");
+    lgSubmenuBtnArrow.classList.add("rotate-180");
     console.log("submenu active");
+  });
+
+  lgSubmenuBtn.addEventListener("mouseleave", () => {
+    lgSubmenu.classList.add("lg:hidden");
+    lgSubmenuBtnArrow.classList.remove("rotate-180");
+    console.log("submenu inactive");
   });
 };
 
