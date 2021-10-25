7d96546091a7bc668a3218a43aad79a295e40b68
Tim Bittins <tech@timbittins.com>
2021-10-25 13:10:20 +0200

    implemented hero-section with video

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        brand: "#22bcbc",
+      },
+    },
+  },
+  variants: {
+    extend: {
+      textColor: ["active", "group-hover"],
+      backgroundColor: ["active", "group-hover"],
+      backgroundColor: ["active", "group-hover"],
+      translate: ["group-hover"],
+    },
+  },
+  plugins: [],
+};
